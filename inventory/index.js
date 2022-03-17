8062ec6c5d5e4be5019e634d9d76ff6a35c8e664
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-03-17 14:56:49 +0600

    message pass with rabbitMQ

--- inventory/index.js
+++ inventory/index.js
@@ -30,3 +30,27 @@ app.get('/', (req, res) => {
 })
 
 app.use('/basic', basicRoutes)
+// amqp = advance message queue protocol
+const amqp = require('amqplib')
+
+connect()
+
+async function connect() {
+  try {
+    const connection = await amqp.connect('amqp://localhost:5672')
+    const channel = await connection.createChannel()
+    // check queue exist or not if not then create
+    const result = await channel.assertQueue('jobs')
+
+    channel.consume('abc', message => {
+      const input = JSON.parse(message.content.toString())
+      console.log(`Received job with input ${input.productId}`)
+
+    })
+
+    console.log('Waiting for messages')
+  } catch (ex) {
+    console.error(ex)
+  }
+}
+
--- order/controllers/orderController.js
+++ order/controllers/orderController.js
@@ -1,6 +1,21 @@
 const orderService = require('../model/orderService')
 
+// amqp = advance message queue protocol
+const amqp = require("amqplib")
+var channel, connection
+async function connect() {
+  try {
+    connection = await amqp.connect("amqp://localhost:5672")
+    channel = await connection.createChannel()
+    // check queue exist or not if not then create
+    await channel.assertQueue("abc")
+  } catch (ex) {
+    console.error(ex)
+  }
+}
+
 const createOrder = async (req, res) => {
+  connect()
   const { productId, productName, quantity, price } = req.body
 
   const newOrderService = {
@@ -11,6 +26,12 @@ const createOrder = async (req, res) => {
   }
 
   const response = await orderService.create(newOrderService)
+
+  await channel.sendToQueue('abc', Buffer.from(JSON.stringify(response)))
+  console.log(`Job sent successfully ${response}`)
+  await channel.close()
+  await connection.close()
+
   return res.status(200).json(response)
 }
 
