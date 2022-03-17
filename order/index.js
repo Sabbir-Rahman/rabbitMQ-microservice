10d436f16980a6e543d46e4594827807d35a51c3
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-03-17 13:56:15 +0600

    database connected

--- order/index.js
+++ order/index.js
@@ -1,11 +1,28 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5002
 const basicRoutes = require('./routers/basicRouter') 
 
-app.listen(PORT,()=> {
+async function connect() {
+  const dbUri = process.env.MONGODB_CONNECTION_URL
+
+  try {
+    await mongoose.connect(dbUri)
+    console.log('Database connected')
+  } catch (error) {
+    console.log(error)
+    console.log('Could not connect to db')
+    process.exit(1)
+  }
+}
+
+app.listen(PORT,async ()=> {
   console.log(`Server running on ${PORT}`)
+  await connect()
 })
 
 app.get('/',(req,res)=> {
